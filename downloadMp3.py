15be06abae80c101f7372fceea42aaed4b8b4c91
ChiatzenW <tokashirizard@gmail.com>
2021-10-07 19:57:40 -0400

    first

---
+++ downloadMp3.py
@@ -0,0 +1,25 @@
+import pytube, os
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+
+def main():
+    if not os.path.exists('tmp'):
+                os.mkdir('tmp')
+
+    #search for a video
+    srch = 'pmaru'
+    s = pytube.Search(srch)
+    yt=s.results[0]
+
+    #download audio-only mp4 with the highest abr
+    yt.streams.filter(type='audio')
+    highest_strm=yt.streams.get_audio_only()
+    mp4_filename=highest_strm.download(output_path='tmp')
+
+    # write to mp3 and remove mp4
+    audio_file = AudioFileClip(mp4_filename)
+    audio_file.write_audiofile(mp4_filename[:-4] + ".mp3")
+    audio_file.close()
+    os.remove(mp4_filename)
+
+if __name__ == "__main__":
+   main()
---
+++ gui.py
@@ -0,0 +1,12 @@
+import downloadMp3
+from kivy.app import App
+from kivy.uix.widget import Widget
+# future: use kivy core audio for playback
+class YmpMenu(Widget):
+    pass
+class YmpApp(App):
+    def build(self):
+        return 
+
+if __name__ == '__main__':
+    YmpApp().run()
